39abe9e9a807be4aec185f34118b97940c92f1ef
babakasse <babakasse@yahoo.fr>
2025-08-26 22:14:39 +0200

    fix-infinit-redirect-on-azure-2

--- public/index.php
+++ public/index.php
@@ -1,21 +1,8 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-// --- Gestion des Trusted Proxies (Azure App Service) ---
-// --- Trusted proxies pour Azure ---
-if ($_SERVER['APP_ENV'] === 'prod') {
-    Request::setTrustedProxies(
-        ['0.0.0.0/0'], // faire confiance à tous les proxies
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_PORT
-    );
-}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
